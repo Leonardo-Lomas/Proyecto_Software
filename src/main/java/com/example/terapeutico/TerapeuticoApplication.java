4591076de1075a69a5e86bd03029845a9e9a7572
Leonardo-Lomas <l.67l89.l3@outlook.com>
2026-04-21 16:14:18 -0600

    Springboot estructura inicial

---
+++ src/main/java/com/example/terapeutico/TerapeuticoApplication.java
@@ -0,0 +1,13 @@
+package com.example.terapeutico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerapeuticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerapeuticoApplication.class, args);
+	}
+
+}
